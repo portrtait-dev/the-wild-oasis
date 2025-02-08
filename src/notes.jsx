5afacf32b2c70b5f1f6baad5b36895c9b5f8e405
type sh,t <olzhas.dossaibek.dev@gmail.com>
2025-02-08 21:40:00 +0500

    💡333. Setting Up Pages and Routes
    - step 1 - install npm package - clear the App.jsx and build from scratch (starter version)
    - step 2 - set up the index Route - add styles
    
    - in this lecture: install React Router and set up our Routes
    - next: build Application Layout

--- src/notes.jsx
+++ src/notes.jsx
@@ -1,10 +1,16 @@
 // Section 25: Setting Up Our Biggest Project + Styled Components [326-335]
 // --------------------------------------------------
 
+// 1/3. Intro to the Project [326-328]
 // 💡326. Section Overview
 // 💡327. Application Planning
 // 💡328. Setting Up the Project: "The Wild Oasis"
+
+// 2/3. Styled Components [329-332]
 // 💡329. Introduction to Styled Components
 // 💡330. Global Styles With Styled Components
 // 💡331. Styled Component Props and the "css" Function
 // 💡332. Building More Reusable Styled Components
+
+// 3/3. App Layout [333-335]
+// 💡333. Setting Up Pages and Routes
